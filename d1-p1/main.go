749d75d651a3da9918867aa7d6249a2da527b0f4
Brian Colvin <brcolvin@gmail.com>
2018-12-10 15:59:28 -0800

    day 1-3

---
+++ d1-p1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"log"
+	"bufio"
+	"fmt"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	total := 0
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		var n int
+		_, err := fmt.Sscanf(s.Text(), "%d", &n)
+		if err != nil {
+			log.Fatalf("Error reading into value")
+		}
+		total += n
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(total)
+}
